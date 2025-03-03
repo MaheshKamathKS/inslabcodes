6788e8e488de535b15c14c7dbafd80ceea78bee1
MaheshKamathKS <158476426+MaheshKamathKS@users.noreply.github.com>
2025-03-03 21:44:58 +0530

    Update file with changes

--- monoCipher.py
+++ monoCipher.py
@@ -1,10 +1,29 @@
-a = input("Enter Text : ")
-s = int(input("Shift : "))
-res= " "
+def mono(plain,key):
+	plain.lower()
+	enc=""
+	for ch in plain:
+		if ch.isalpha():
+			enc+=key[ord(ch)-97]
+	return enc
 
-for i in a:
-    if(i.isupper()):
-        res += chr((ord(i)+s-65)%26+65)
-    elif(i.islower()):
-        res += chr((ord(i)+s-97)%26+97)
-print(f"Ciphertext : {res}")
+def decrypt(enc,key):
+	dec=""
+	for ch in enc:
+		for i,chs in enumerate(key):
+			if ch==chs:
+				dec+=chr(ord('a')+i)
+	return dec
+
+plain=input("plain : ")
+y=input("keytext : ")
+key=y.upper()
+
+for i in range(26):
+	ch=chr(ord('A')+i)
+	if ch not in key:
+		key+=ch
+print("key : ",key)
+enc=mono(plain,key)
+dec=decrypt(enc,key)
+print(enc)
+print(dec)
